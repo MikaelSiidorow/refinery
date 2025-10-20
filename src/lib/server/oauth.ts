4c38ed7c3dc41c6487172896fc3879e4f908f57a
Mikael Siidorow <mikael@siidorow.com>
2025-10-20 22:11:42 +0300

    fix: get github redirect url from env

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,11 +1,4 @@
 import { GitHub } from 'arctic';
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
-import { dev } from '$app/environment';
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_REDIRECT_URL } from '$env/static/private';
 
-export const github = new GitHub(
-	GITHUB_CLIENT_ID,
-	GITHUB_CLIENT_SECRET,
-	dev
-		? 'http://localhost:5173/sign-in/github/callback'
-		: 'https://yourdomain.com/sign-in/github/callback'
-);
+export const github = new GitHub(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_REDIRECT_URL);
