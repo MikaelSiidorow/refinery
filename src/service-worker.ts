8d69732f7802a87ca53588f7bfc4e50f2c20d707
Mikael Siidorow <mikael@siidorow.com>
2025-10-25 15:45:07 +0300

    fix: format and fix service worker types

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,14 +1,22 @@
+// Disables access to DOM typings like `HTMLElement` which are not available
+// inside a service worker and instantiates the correct globals
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+
+// Ensures that the `$service-worker` import has proper type definitions
 /// <reference types="@sveltejs/kit" />
+
+// Only necessary if you have an import from `$env/static/public`
+/// <reference types="../.svelte-kit/ambient.d.ts" />
+
 import { build, files, version } from '$service-worker';
 
 declare const self: ServiceWorkerGlobalScope;
 
 const CACHE = `cache-${version}`;
 
-const ASSETS = [
-	...build,
-	...files
-];
+const ASSETS = [...build, ...files];
 
 self.addEventListener('install', (event) => {
 	async function addFilesToCache() {
