403832a1ff6e2beb7b5ed8d4877429bdd4c089b2
Mikael Siidorow <mikael@siidorow.com>
2025-12-27 02:36:17 +0200

    fix: optimize lucide icons with vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import lucidePreprocess from 'vite-plugin-lucide-preprocess';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	plugins: [lucidePreprocess(), tailwindcss(), sveltekit(), devtoolsJson()],
 	server: {
 		host: '127.0.0.1',
 		port: 5173,
